67c35a5acdc0f8ae7e40b4d33d3d4dc5ffad99f8
Daniel Whitenack <whitenack.daniel@gmail.com>
2022-04-29 17:47:17 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+# Creates the FastAPI app object
+def create_app():
+
+    app = FastAPI()
+
+    @app.get("/")
+    def read_root():
+        return {"Hello": "World"}
+
+    return app
---
+++ app_test.py
@@ -0,0 +1,21 @@
+import app
+import pytest
+from fastapi.testclient import TestClient
+
+# Create a generator that when called gives
+# a mock/ test API client, for our FastAPI app.
+@pytest.fixture
+def client():
+
+    # Get a mock FastAPI app.
+    mock_app = app.create_app()
+
+    # Yield the mock/ test client for the FastAPI
+    # app we spun up any time this generator is called.
+    with TestClient(mock_app) as client:
+        yield client
+
+def test_read_main(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
