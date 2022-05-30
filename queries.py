8265b0b6d0ffe56a758b10f027c44067c8a3b870
Daniel Whitenack <whitenack.daniel@gmail.com>
2022-05-30 13:04:08 -0400

    update to Hasura-based setup, GraphQL auth

--- queries.py
+++ queries.py
@@ -2,14 +2,14 @@ class all_queries():
     def list_versions_query():
         list_version = """
                     query MyQuery {
-                        queryBibleVersion {
+                        bible_version {
                             id
                             name
                             abbreviation
-                            isoLanguage {
+                            iso_language {
                                 iso639
                             }
-                            isoScript {
+                            iso_script {
                                 iso15924
                             }
                             rights
