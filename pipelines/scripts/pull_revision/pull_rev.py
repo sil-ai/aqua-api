2a6a5356c8d2e36fc2a4099c44488a9333656e69
lohm <lightofhopemedia@gmail.com>
2022-09-29 09:03:49 +0400

    added root logger for now and updated tests

--- pipelines/scripts/pull_revision/pull_rev.py
+++ pipelines/scripts/pull_revision/pull_rev.py
@@ -5,25 +5,13 @@ import logging
 import pandas as pd
 from db_connect import get_session, VerseText
 
-def get_logger():
-    module_name = __file__.split('/')[-1].split('.')[0]
-    #set the root logger to a debug level
-    logger = logging.getLogger(module_name)
-    logger.setLevel(logging.DEBUG)
-    logger.handlers = []
-    #set up a stream handler
-    s_handler = logging.StreamHandler()
-    s_handler.setLevel(logging.DEBUG)
-    log_format = logging.Formatter(fmt='%(asctime)s | %(levelname)s | %(message)s | %(name)s',
-                                 datefmt='%Y-%m-%d %H:%M:%S')
-    s_handler.setFormatter(log_format)
-    logger.addHandler(s_handler)
-    return logger
+#!!! generic root logger for now
+logging.getLogger().level == logging.DEBUG
 
 class PullRevision:
 
     def __init__(self,):
-        self.logger = get_logger()
+        #self.logger = get_logger()
         #gets the args of the form --revision 3 --out /path/to/output/file
         args = self.get_args()
         if not (args.revision and args.out):
@@ -65,7 +53,8 @@ class PullRevision:
         #with postgres connection gets the verses from the verseText table
         #??? Think about dividing get_session into get_engine and get_session
         __,session = next(get_session())
-        self.logger.info('Loading verses from Revision %s...', self.revision_id)
+        #self.logger.info('Loading verses from Revision %s...', self.revision_id)
+        logging.info('Loading verses from Revision %s...', self.revision_id)
         #builds a dataframe of verses from the revision in self.revision_id
         revision_verses = pd.read_sql(session.query(VerseText)\
                           .filter(VerseText.bibleRevision==self.revision_id)\
@@ -77,9 +66,11 @@ class PullRevision:
                 #loads the verses as part of the PullRevision object
                 self.revision_text = revision_verses.set_index('id',drop=True)
             else:
-                self.logger.info('Duplicated verses in Revision %s', self.revision_id)
+                #self.logger.info('Duplicated verses in Revision %s', self.revision_id)
+                logging.info('Duplicated verses in Revision %s', self.revision_id)
         else:
-            self.logger.info('No verses for Revision %s', self.revision_id)
+            #self.logger.info('No verses for Revision %s', self.revision_id)
+            logging.info('No verses for Revision %s', self.revision_id)
         return self
 
     def prepare_output(self):
@@ -104,10 +95,13 @@ class PullRevision:
             with open(filepath,'w') as outfile:
                 for verse_text in output_text:
                     outfile.write(f'{verse_text}\n')
-            self.logger.info('Revision %s saved to file %s in location %s',
-                              self.revision_id, filename, self.out)
+            #self.logger.info('Revision %s saved to file %s in location %s',
+            #                  self.revision_id, filename, self.out)
+            logging.info('Revision %s saved to file %s in location %s',
+                          self.revision_id, filename, self.out)
         else:
-            self.logger.info('Revision text is empty. Nothing printed.')
+            #self.logger.info('Revision text is empty. Nothing printed.')
+            logging.info('Revision text is empty. Nothing printed.')
 
 if __name__ == '__main__':
     try:
@@ -117,7 +111,4 @@ if __name__ == '__main__':
     except (ValueError, OSError,
             KeyError, AttributeError,
             FileNotFoundError) as err:
-        try:
-            pr.logger.error(err)
-        except NameError:
-            print(err)
+        logging.error(err)
