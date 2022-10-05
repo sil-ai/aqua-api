1e1954ff0ffe6074fee004f9dbbefd905b91bc6d
cassiep429 <cassandra.powell@mail.mcgill.ca>
2022-10-05 09:41:07 -0400

    Clean code

--- pipelines/scripts/word_alignment/align.py
+++ pipelines/scripts/word_alignment/align.py
@@ -1,4 +1,4 @@
-#imports
+# imports
 import argparse
 import string
 import os
@@ -8,43 +8,49 @@ from tqdm import tqdm
 from machine.corpora import TextFileTextCorpus
 from machine.tokenization import LatinWordTokenizer
 from machine.translation import SymmetrizationHeuristic
-from machine.translation.thot import ThotFastAlignWordAlignmentModel, ThotSymmetrizedWordAlignmentModel
+from machine.translation.thot import (
+    ThotFastAlignWordAlignmentModel,
+    ThotSymmetrizedWordAlignmentModel,
+)
+
 
 def write_condensed_files(src_file, trg_file):
-    #open files
+    # open files
     with open(src_file) as f:
         src_data = f.readlines()
     with open(trg_file) as f:
         trg_data = f.readlines()
-    
-    #make into df
-    df = pd.DataFrame({'src':src_data, 'trg':trg_data})
 
-    #remove lines that contain \n in either src or trg
-    df = df[df.src != '\n']
-    df = df[df.trg != '\n']
+    # make into df
+    df = pd.DataFrame({"src": src_data, "trg": trg_data})
 
-    #remove punctuation
-    df['src'] = df['src'].str.replace('[{}]'.format(string.punctuation), '', regex=True)
-    df['trg'] = df['trg'].str.replace('[{}]'.format(string.punctuation), '', regex=True)
+    # remove lines that contain \n in either src or trg
+    df = df[df.src != "\n"]
+    df = df[df.trg != "\n"]
 
-    #make lowercase
-    df['src'] = df['src'].str.lower()
-    df['trg'] = df['trg'].str.lower()
+    # remove punctuation
+    df["src"] = df["src"].str.replace("[{}]".format(string.punctuation), "", regex=True)
+    df["trg"] = df["trg"].str.replace("[{}]".format(string.punctuation), "", regex=True)
 
-    #write to condensed txt files
+    # make lowercase
+    df["src"] = df["src"].str.lower()
+    df["trg"] = df["trg"].str.lower()
+
+    # write to condensed txt files
     with open("src_condensed.txt", "w") as f:
-        for line in df['src']:
+        for line in df["src"]:
             f.write(line)
     with open("trg_condensed.txt", "w") as f:
-        for line in df['trg']:
+        for line in df["trg"]:
             f.write(line)
 
 
 def create_corpus(src_file, trg_file):
     source_corpus = TextFileTextCorpus(src_file)
     target_corpus = TextFileTextCorpus(trg_file)
-    parallel_corpus = source_corpus.align_rows(target_corpus).tokenize(LatinWordTokenizer())
+    parallel_corpus = source_corpus.align_rows(target_corpus).tokenize(
+        LatinWordTokenizer()
+    )
     return parallel_corpus
 
 
@@ -54,28 +60,36 @@ def train_model(corpus):
     symmetrized_model = ThotSymmetrizedWordAlignmentModel(src_trg_model, trg_src_model)
     symmetrized_model.heuristic = SymmetrizationHeuristic.GROW_DIAG_FINAL_AND
     trainer = symmetrized_model.create_trainer(corpus.lowercase())
-    trainer.train(lambda status: print(f"Training Symmetrized FastAlign model: {status.percent_completed:.2%}"))
+    trainer.train(
+        lambda status: print(
+            f"Training Symmetrized FastAlign model: {status.percent_completed:.2%}"
+        )
+    )
     trainer.save()
     return symmetrized_model
 
 
 def get_alignments(model, corpus, vrefs):
     alignments = model.get_best_alignment_batch(corpus.lowercase().to_tuples())
-    data = {'vref':[], 'source':[], 'target':[], 'word score':[], 'verse score':[]}
+    data = {"vref": [], "source": [], "target": [], "word score": [], "verse score": []}
     print("Getting alignments...")
     c = 0
     for source_segment, target_segment, alignment in tqdm(alignments):
         pair_indices = alignment.to_aligned_word_pairs()
-        verse_score = model.get_avg_translation_score(source_segment, target_segment, alignment)
+        verse_score = model.get_avg_translation_score(
+            source_segment, target_segment, alignment
+        )
         vref = vrefs[c]
         c = c + 1
         for pair in pair_indices:
-            score = model.get_translation_score(source_segment[pair.source_index], target_segment[pair.target_index])
-            data['source'].append(source_segment[pair.source_index])
-            data['target'].append(target_segment[pair.target_index])
-            data['word score'].append(score)
-            data['verse score'].append(verse_score)
-            data['vref'].append(vref)
+            score = model.get_translation_score(
+                source_segment[pair.source_index], target_segment[pair.target_index]
+            )
+            data["source"].append(source_segment[pair.source_index])
+            data["target"].append(target_segment[pair.target_index])
+            data["word score"].append(score)
+            data["verse score"].append(verse_score)
+            data["vref"].append(vref)
     df = pd.DataFrame(data)
     return df
 
@@ -88,45 +102,57 @@ def get_vrefs(src_file, trg_file, is_bible):
         trg_data = f.readlines()
 
     if is_bible == "True":
-        with open('../../../fixtures/vref.txt', 'r') as f:
+        with open("../../../fixtures/vref.txt", "r") as f:
             vrefs = f.readlines()
         vrefs = [line.strip() for line in vrefs]
     else:
         vrefs = [str(i) for i in range(len(src_data))]
 
-    df = pd.DataFrame({'vref':vrefs, 'src':src_data, 'trg':trg_data})
-    df = df[df.src != '\n']
-    df = df[df.trg != '\n']
-    return df['vref'].tolist()
+    df = pd.DataFrame({"vref": vrefs, "src": src_data, "trg": trg_data})
+    df = df[df.src != "\n"]
+    df = df[df.trg != "\n"]
+    return df["vref"].tolist()
 
 
 def get_vref_scores(df):
-    #remove duplicate verses
-    df = df.drop_duplicates(subset=['vref'])
-    vref_df = df[['vref', 'verse score']]
+    # remove duplicate verses
+    df = df.drop_duplicates(subset=["vref"])
+    vref_df = df[["vref", "verse score"]]
     return vref_df
 
 
 def apply_threshold(df, threshold):
     # remove duplicates and average out verse and word scores
-    dups = df.groupby(['source', 'target']).size().reset_index()
-    avgs = df.groupby(['source', 'target']).mean().reset_index()
+    dups = df.groupby(["source", "target"]).size().reset_index()
+    avgs = df.groupby(["source", "target"]).mean().reset_index()
     no_dups = pd.merge(dups, avgs)
     no_dups.rename(columns={0: "align_count"}, inplace=True)
 
-    #apply threshold
-    no_dups = no_dups[no_dups['word score'] >= threshold]
+    # apply threshold
+    no_dups = no_dups[no_dups["word score"] >= threshold]
     return no_dups
 
 
 def run_align(src_file, trg_file, threshold, outpath, is_bible):
-    #remove empty lines
+    # get file names
+    source_name = os.path.basename(src_file)
+    target_name = os.path.basename(trg_file)
+    path = (
+        outpath
+        + "/"
+        + source_name.split(".")[0]
+        + "_"
+        + target_name.split(".")[0]
+        + "_align"
+    )
+
+    # remove empty lines
     write_condensed_files(src_file, trg_file)
 
-    #get vrefs
+    # get vrefs
     vrefs = get_vrefs(src_file, trg_file, is_bible)
 
-    #create parallel corpus
+    # create parallel corpus
     parallel_corpus = create_corpus("src_condensed.txt", "trg_condensed.txt")
 
     # Train fast_align model
@@ -141,12 +167,7 @@ def run_align(src_file, trg_file, threshold, outpath, is_bible):
     # Apply threshold
     no_dups = apply_threshold(df, threshold)
 
-    #write results to csv
-    source_name = os.path.basename(src_file)
-    target_name = os.path.basename(trg_file)
-    path = outpath + "/" + source_name.split('.')[0] + "_" + target_name.split('.')[0] + "_align"
-    
-    #if dir doesn't exist, create it
+    # if dir doesn't exist, create it
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -154,19 +175,21 @@ def run_align(src_file, trg_file, threshold, outpath, is_bible):
     df.to_csv(path + "/in_context.csv")
     vref_df.to_csv(path + "/vref_scores.csv")
 
-    #delete temp files
+    # delete temp files
     os.remove("src_condensed.txt")
     os.remove("trg_condensed.txt")
 
 
 if __name__ == "__main__":
-    #command line args
+    # command line args
     parser = argparse.ArgumentParser(description="Argparser")
-    parser.add_argument('--source', type=str, help='source translation')
-    parser.add_argument('--target', type=str, help='target translation')
-    parser.add_argument('--threshold', type=float, default=0.5, help='word score threshold {0,1}')
-    parser.add_argument('--outpath', type=str, help='where to write results')
-    parser.add_argument('--is-bible', type=str, default='False', help='is bible data?')
+    parser.add_argument("--source", type=str, help="source translation")
+    parser.add_argument("--target", type=str, help="target translation")
+    parser.add_argument(
+        "--threshold", type=float, default=0.5, help="word score threshold {0,1}"
+    )
+    parser.add_argument("--outpath", type=str, help="where to write results")
+    parser.add_argument("--is-bible", type=str, default="False", help="is bible data?")
     args, unknown = parser.parse_known_args()
-    
+
     run_align(args.source, args.target, args.threshold, args.outpath, args.is_bible)
--- pipelines/scripts/word_alignment/combined.py
+++ pipelines/scripts/word_alignment/combined.py
@@ -1,39 +1,50 @@
-#imports
+# imports
 import os
 import json
-import argparse 
+import argparse
 
 import pandas as pd
 import align
 import match
 
-#make ouput directory
+# make ouput directory
 def make_output_dir(source, target, outpath):
-    s = source.split('/')[-1].split('.')[0]
-    t = target.split('/')[-1].split('.')[0]
-    path = str(f'{outpath}/{s}_{t}_combined')
+    s = source.split("/")[-1].split(".")[0]
+    t = target.split("/")[-1].split(".")[0]
+    path = str(f"{outpath}/{s}_{t}_combined")
     os.makedirs(path, exist_ok=True)
     return s, t, path
 
-#run fast_align
+
+# run fast_align
 def run_fa(source, target, word_score_threshold, path, is_bible):
     align.run_align(source, target, word_score_threshold, path, is_bible)
 
 
-#run match words
-def run_match_words(source, target, path, jaccard_similarity_threshold, count_threshold):
-    match.run_match(source, target, path, 'INFO', jaccard_similarity_threshold, count_threshold, True)
+# run match words
+def run_match_words(
+    source, target, path, jaccard_similarity_threshold, count_threshold
+):
+    match.run_match(
+        source,
+        target,
+        path,
+        "INFO",
+        jaccard_similarity_threshold,
+        count_threshold,
+        True,
+    )
 
 
-#combine results
+# combine results
 def combine_df(outpath, s, t):
-    #open results
-    align_path = str(f'{outpath}/{s}_{t}_align/sorted.csv')
-    match_path = str(f'{outpath}/{s}_{t}_match/{s}-{t}-dictionary.json')
+    # open results
+    align_path = str(f"{outpath}/{s}_{t}_align/sorted.csv")
+    match_path = str(f"{outpath}/{s}_{t}_match/{s}-{t}-dictionary.json")
     fa_results = pd.read_csv(align_path)
     match_results = json.load(open(match_path))
 
-    #explode the match data
+    # explode the match data
     sources = []
     targets = []
     jac_sims = []
@@ -42,50 +53,70 @@ def combine_df(outpath, s, t):
     for lemma in match_results:
         for features in match_results[lemma]:
             sources.append(lemma)
-            targets.append(features['value'])
-            jac_sims.append(features['jaccard_similarity'])
-            counts.append(features['count'])
-            
+            targets.append(features["value"])
+            jac_sims.append(features["jaccard_similarity"])
+            counts.append(features["count"])
+
     data = {
-        'source':sources,
-        'target':targets,
-        'jac_sim':jac_sims,
-        'match_count':counts,
+        "source": sources,
+        "target": targets,
+        "jac_sim": jac_sims,
+        "match_count": counts,
     }
 
-    #write to df and merge with fa results
+    # write to df and merge with fa results
     match_results = pd.DataFrame(data)
-    df = pd.merge(fa_results, match_results, how='left', on=['source', 'target']).fillna(-1)
-    df.drop(columns=['Unnamed: 0'], inplace=True)
+    df = pd.merge(
+        fa_results, match_results, how="left", on=["source", "target"]
+    ).fillna(-1)
+    df.drop(columns=["Unnamed: 0"], inplace=True)
     return df
 
 
 if __name__ == "__main__":
     # #command line args
     parser = argparse.ArgumentParser(description="Argparser")
-    parser.add_argument('--source', type=str, help='source bible')
-    parser.add_argument('--target', type=str, help='target bible')
-    parser.add_argument('--word-score-threshold', type=float, default=0.5, help='word score threshold {0,1}')
-    parser.add_argument('--jaccard-similarity-threshold', type=float, help="Threshold for Jaccard Similarity score to be significant", default=0.5)
-    parser.add_argument('--is-bible', type=str, default="False", help='is bible')
-    parser.add_argument('--count-threshold', type=int, help="Threshold for count (number of co-occurences) score to be significant", default=1)
-    parser.add_argument('--outpath', type=str, help='where to store results')
+    parser.add_argument("--source", type=str, help="source bible")
+    parser.add_argument("--target", type=str, help="target bible")
+    parser.add_argument(
+        "--word-score-threshold",
+        type=float,
+        default=0.5,
+        help="word score threshold {0,1}",
+    )
+    parser.add_argument(
+        "--jaccard-similarity-threshold",
+        type=float,
+        help="Threshold for Jaccard Similarity score to be significant",
+        default=0.5,
+    )
+    parser.add_argument("--is-bible", type=str, default="False", help="is bible")
+    parser.add_argument(
+        "--count-threshold",
+        type=int,
+        help="Threshold for count (number of co-occurences) score to be significant",
+        default=1,
+    )
+    parser.add_argument("--outpath", type=str, help="where to store results")
     args, unknown = parser.parse_known_args()
 
-    #make output dir
+    # make output dir
     s, t, path = make_output_dir(args.source, args.target, args.outpath)
 
-    #run fast align
+    # run fast align
     run_fa(args.source, args.target, args.word_score_threshold, path, args.is_bible)
 
-    #run match words
-    run_match_words(args.source, args.target, path, args.jaccard_similarity_threshold, args.count_threshold)
+    # run match words
+    run_match_words(
+        args.source,
+        args.target,
+        path,
+        args.jaccard_similarity_threshold,
+        args.count_threshold,
+    )
 
-    #combine results
+    # combine results
     df = combine_df(path, s, t)
 
-    #save results
+    # save results
     df.to_csv(f"{path}/{s}_{t}_combined.csv")
-
-
-
