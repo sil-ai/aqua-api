ea160bb07de8f4578a259c7509f13ce5562bef3a
lohm <lightofhopemedia@gmail.com>
2022-10-10 15:56:21 +0400

    pytest works with the new split_revision

--- pipelines/scripts/split_revision/test_split_revision.py
+++ pipelines/scripts/split_revision/test_split_revision.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 import pytest
 from split_revision import SplitRevision
 from mock import patch
@@ -9,56 +10,32 @@ from datetime import datetime
 
 @pytest.fixture(scope='session')
 def revision_filepath():
-    return 'sample_test_revision.txt'
+    return '2_3_merge.csv'
 
-#test for missing input file
-def test_missing_input():
+@pytest.mark.parametrize('input,num,out',
+                        [(None,100,'.'),
+                         ('./input.txt',None,'.'),
+                         ('./input.txt',5,None)],
+                          ids=['input','num','out'])
+#test for missing path
+def test_missing_path(input, num, out):
     try:
         with patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input=None,num=100,out='.')):
+            return_value=argparse.Namespace(input=input, num=num, out=out)):
             SplitRevision()
-            raise AssertionError('Missing input should have failed')
+        raise AssertionError('Missing path should have failed')
     except ValueError as err:
-        if err.args[0] == 'Missing split number or path':
-            pass
-        else:
-            raise AssertionError(f'Error is {err}') from err
+        assert err.args[0] == 'Missing split number or path', f'Error is {err}'
 
 #test for invalid input file
 def test_invalid_input():
     try:
         with patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input='./input.txt',num=5,out='.')):
+            return_value=argparse.Namespace(input='./input.txt', num=5, out='.')):
             SplitRevision()
         raise AssertionError('Invalid filename should have failed')
     except FileNotFoundError as err:
         pass
-
-#test for invalid input file
-def test_missing_split_number():
-    try:
-        with patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input='./input.txt',num=None,out='.')):
-            SplitRevision()
-        raise AssertionError('Missing split number should have failed')
-    except ValueError as err:
-        if err.args[0] == 'Missing split number or path':
-            pass
-        else:
-            raise AssertionError(f'Error is {err}') from err
-
-#test for missing output
-def test_missing_output():
-    try:
-        with patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input='./input.txt',num=5,out=None)):
-            SplitRevision()
-        raise AssertionError('Missing output should have failed')
-    except ValueError as err:
-        if err.args[0] == 'Missing split number or path':
-            pass
-        else:
-            raise AssertionError(f'Error is {err}') from err
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -68,7 +45,7 @@ def get_args():
 def test_split_revision_instance(revision_filepath, valuestorage):
     try:
         with patch('argparse.ArgumentParser.parse_args',
-            return_value=argparse.Namespace(input=revision_filepath,num=100,out='.')):
+            return_value=argparse.Namespace(input=revision_filepath, num=100, out='.')):
             sr = SplitRevision()
             args = get_args()
             #assert that the args are correct in the instance
@@ -76,8 +53,8 @@ def test_split_revision_instance(revision_filepath, valuestorage):
             assert sr.output_filepath == args['out']
             assert sr.num == args['num']
             #assert that the revision_list file transferred properly after taking out blanks
-            revision_file = open(revision_filepath).read()
-            assert len(sr.revision_list) == len([item for item in revision_file.splitlines() if item])
+            revision_file = pd.read_csv(revision_filepath)
+            assert len(sr.revision_df) == len(revision_file)
             #save this instance to valuestorage for further tests
             valuestorage.valid_split_rev = sr
     except Exception as err:
@@ -94,9 +71,9 @@ def test_split_revision(valuestorage):
         assert len(split_revision) == sr.num
         #make sure the individual chunks add up to the total number of the verses
         #in the revision
-        assert sum([len(item)for item in split_revision]) == len(sr.revision_list)
+        assert sum([len(item)for item in split_revision]) == len(sr.revision_df)
         #make sure that the chunks are the same size
-        average_chunk_length = len(sr.revision_list)//sr.num
+        average_chunk_length = len(sr.revision_df)//sr.num
         assert all([(len(item)==average_chunk_length)\
                or (len(item)==average_chunk_length+1)\
                   for item in split_revision])
@@ -121,44 +98,49 @@ def test_split_revision_output(valuestorage, tmp_path):
     except Exception as err:
         raise AssertionError(f'Error is {err}') from err
 
-@pytest.mark.parametrize('chunk,line,expected',
- [(1,5,'GEN 15:12,"And when the sun was going down, a deep sleep fell upon Abram; and, lo, an horror of great darkness fell upon him."'),
-  (50,10,'ISA 36:3,"Then came forth unto him Eliakim, Hilkiah’s son, which was over the house, and Shebna the scribe, and Joah, Asaph’s son, the recorder."'),
-  (75,8,'ACT 15:40,"And Paul chose Silas, and departed, being recommended by the brethren unto the grace of God."'),
-  (89,3,'SIR 18:31,"If thou givest thy soul the desires that please her, she will make thee a laughingstock to thine enemies that malign thee."')],
-  ids=range(4))
-def test_chunked_file_contents(chunk,line,expected,valuestorage):
+@pytest.mark.parametrize('chunk,line,expected_ref,expected_text',
+  [(0,15,'GEN 1:16','And God made two great lights; the greater light to rule the day, and the lesser light to rule the night: he made the stars also.'),
+   (42,14,'GEN 40:18','And Joseph answered and said, This is the interpretation thereof: The three baskets are three days:'),
+   (81,13,'EXO 26:11','And thou shalt make fifty taches of brass, and put the taches into the loops, and couple the tent together, that it may be one.'),
+   (97,25,'EXO 39:26','A bell and a pomegranate, a bell and a pomegranate, round about the hem of the robe to minister in; as the LORD commanded Moses.')],
+   ids=['GEN 1:16','GEN 40:18','EXO 26:11','EXO 39:26'])
+def test_chunked_file_contents(chunk,line,
+                               expected_ref,
+                               expected_text,
+                               valuestorage):
     regex = re.compile(r'chunk(.*)\.csv')
     list_of_chunked_files = valuestorage.list_of_chunked_files
     try:
         this_chunk_file = list(filter(lambda item:regex.search(str(item)).groups()[0]==str(chunk), list_of_chunked_files))[0]
-        this_chunk = open(this_chunk_file).read()
-        this_line = this_chunk.splitlines()[line]
-        assert this_line == expected
+        this_chunk = pd.read_csv(this_chunk_file)
+        this_line = this_chunk.iloc[line]
+        assert this_line['vref'] == expected_ref
+        assert this_line['target'] == expected_text
+        assert this_line['reference'] == expected_text
     except IndexError:
         raise AssertionError('Chunk {chunk} has no file output')
 
-#test for working logger
-def test_working_logger(caplog, valuestorage):
-    splitrev = valuestorage.valid_split_rev
-    #generate the log entry
-    splitrev.split_revision()
-    #check the logs
-    current_dt = datetime.now()
-    logentry = caplog.records[-1]
-    print(logentry)
-    #get the utc logentry
-    log_dt = datetime.fromtimestamp(logentry.created)
-    #compare the date
-    assert current_dt.date() == log_dt.date()
-    #compare the hour after converting current_dt to utc
-    assert current_dt.hour == log_dt.hour
-    #compare the minute
-    assert abs(current_dt.minute - log_dt.minute) <=1
-    #compare the logging level
-    assert logentry.levelname == 'INFO'
-    #compare the message
-    assert  'Splitting revision' in logentry.message
-    assert str(splitrev.num) in logentry.message
-    #compare the module
-    assert logentry.module == 'split_revision'
+# #test for working logger
+# def test_working_logger(caplog, valuestorage):
+#     splitrev = valuestorage.valid_split_rev
+#     #generate the log entry
+#     splitrev.split_revision()
+#     #check the logs
+#     current_dt = datetime.now()
+#     logentry = caplog.records[-1]
+#     print(logentry)
+#     #get the utc logentry
+#     log_dt = datetime.fromtimestamp(logentry.created)
+#     #compare the date
+#     assert current_dt.date() == log_dt.date()
+#     #compare the hour after converting current_dt to utc
+#     assert current_dt.hour == log_dt.hour
+#     #compare the minute
+#     assert abs(current_dt.minute - log_dt.minute) <=1
+#     #compare the logging level
+#     assert logentry.levelname == 'INFO'
+#     #compare the message
+#     assert  'Splitting revision' in logentry.message
+#     assert str(splitrev.num) in logentry.message
+#     #compare the module
+#     assert logentry.module == 'split_revision'
