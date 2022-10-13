18859a3e9a6056e796212c90da10891ee34bd944
Mark <woodwardmw@gmail.com>
2022-10-13 15:00:37 -0700

    Updated pachyderm.py to be consistent with other changes to file paths, added command-line args

--- pipelines/scripts/word_alignment/pachyderm.py
+++ pipelines/scripts/word_alignment/pachyderm.py
@@ -1,57 +1,53 @@
 from pathlib import Path
 import logging
-from combined import run_fa, run_match_words, combine_df
+from combined import run_fa, run_match_words, run_combine_results
 import os
+import argparse
 
-def main():
-    jaccard_similarity_threshold = 0.01
-    count_threshold = 0
-    is_bible = True
-    refresh_cache = False
-    outpath = Path('pfs/out')
-    ref_dir = Path('pfs/ref_data')
-    for dirpath, dirs, files in os.walk("pfs/text_data"):
-        for file in files:
-            print(f"File 1: {file}")
+def main(args):
 
-            file = Path(file)
-            for ref_dirpath, _, ref_files in os.walk(ref_dir):
-                for ref_file in ref_files:
-                    if ref_file != 'vref.txt':
-                        print(f"File 2: {ref_file}")
+    jaccard_similarity_threshold = args.jaccard_similarity_threshold
+    count_threshold = args.count_threshold
+    is_bible = args.is_bible
+    refresh_cache = args.refresh_cache
+    outpath = args.outpath
+    ref_dir = args.ref_dir
+    data_dir = args.data_dir
+    for file in data_dir.iterdir():
+        print(f"File 1: {file}")
 
-                        ref_file = Path(ref_file)
-                        if file != ref_file:
-                            out_file = outpath / f'{file.stem}_{ref_file.stem}_match' / f'{file.stem}-{ref_file.stem}_ref_df.csv'
-                            print(out_file)
-                            if not out_file.exists():
-                                run_fa(
-                                    Path(dirpath) / file, 
-                                    Path(ref_dirpath) / ref_file, 
+        file = Path(file)
+        for ref_file in ref_dir.iterdir():
+            if ref_file.stem != 'vref':
+                print(f"File 2: {ref_file}")
+                ref_file = Path(ref_file)
+                outpath = outpath / f"{file.stem}_{ref_file.stem}"
+                if file != ref_file:
+                    run_fa(
+                        file, 
+                        ref_file, 
+                        outpath, 
+                        is_bible=is_bible,
+                        )
+                    run_match_words(
+                                    file, 
+                                    ref_file, 
                                     outpath, 
-                                    is_bible=is_bible,
+                                    jaccard_similarity_threshold=jaccard_similarity_threshold, 
+                                    count_threshold=count_threshold,
+                                    refresh_cache=refresh_cache,
                                     )
-                            run_match_words(
-                                            Path(dirpath) / file, 
-                                            Path(ref_dirpath) / ref_file, 
-                                            outpath, 
-                                            jaccard_similarity_threshold=jaccard_similarity_threshold, 
-                                            count_threshold=count_threshold,
-                                            refresh_cache=refresh_cache,
-                                            )
-                        df = combine_df(outpath, file.stem, ref_file.stem)
-                        # reverse_df = combine_df(outpath, ref_file.stem, file.stem)
-
-                        #save results
-                        path = outpath / f'{file.stem}_{ref_file.stem}_combined'
-                        if not path.exists():
-                            path.mkdir(exist_ok=True)
-                        # reverse_path = outpath / f'{ref_file.stem}_{file.stem}_combined'
-                        # if not reverse_path.exists():
-                        #     reverse_path.mkdir(exist_ok=True)
-                        df.to_csv(path / f'{file.stem}_{ref_file.stem}_combined.csv')
-                        # reverse_df.to_csv(reverse_path / f'{ref_file.stem}_{file.stem}_combined.csv')
+                run_combine_results(outpath)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", type=Path, default=Path("/pfs/text_data"), help="Directory for input (source) files")
+    parser.add_argument("--ref-dir", type=Path, default=Path('/pfs/ref_data'), help="Directory for reference (target) files")
+    parser.add_argument("--outpath", type=Path, default=Path("/pfs/out"), help="Output directory")
+    parser.add_argument("--jaccard-similarity-threshold", default=0.0, type=float, help="Jaccard Similarity threshold for including matches in dictionary")
+    parser.add_argument("--count-threshold", type=int, default=0, help="Count threshold for including matches in dictionary")
+    parser.add_argument("--is-bible", action="store_true", help="Whether text is Bible, in which case the length of the text file must be 41,899 lines")
+    parser.add_argument("--refresh-cache", action="store_true", help="Refresh and overwrite the existing cache",)
+    args = parser.parse_args()
+    main(args)
