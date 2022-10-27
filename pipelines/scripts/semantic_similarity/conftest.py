e5d1209317ed06f67f7ef4e5842a27ff75a074c3
lohm <lightofhopemedia@gmail.com>
2022-10-27 14:52:36 +0400

    working on real data with modifications to test_sem_sim

--- pipelines/scripts/semantic_similarity/conftest.py
+++ pipelines/scripts/semantic_similarity/conftest.py
@@ -3,11 +3,11 @@ import pytest
 
 @pytest.fixture(scope='session')
 def valid_paths():
-    return {'input': '2_3_merge_chunk42.csv', 'out': '.'}
+    return {'input': '1_2_merge_chunk42.csv', 'out': '.'}
 
 @pytest.fixture(scope='session')
 def json_output():
-    return json.load(open('2_3_semsim_10_13.json'))
+    return json.load(open('1_2_semsim_chunk42_10_27.json'))
 
 class ValueStorage:
     valid_sem_sim = None
