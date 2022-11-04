3a0642971e7ab6659c196f7d8c5b42c7a7ec74c1
lohm <lightofhopemedia@gmail.com>
2022-11-04 09:40:12 +0400

    updates to db_connect string

--- pipelines/scripts/pull_revision/db_connect.py
+++ pipelines/scripts/pull_revision/db_connect.py
@@ -20,7 +20,7 @@ def get_session():
     #??? Should this yield only session?
     #Need engine in testing but maybe I can pull out of session?
     try:
-        engine = create_engine(os.environ['AQUA_DB'], pool_size=5, pool_recycle=3600)
+        engine = create_engine(os.environ['AQUA_CONNECTION_STRING'], pool_size=5, pool_recycle=3600)
     except KeyError as err:
         raise KeyError(f'Missing environmental variable {err}') from err
     with Session(engine) as session:
