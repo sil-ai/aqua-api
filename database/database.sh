cb9948ecc3bf8744104685356dcc6839c0d1fc03
jmansdorfer <90076431+jmansdorfer@users.noreply.github.com>
2022-11-08 08:45:09 -0500

    Update database.sh

--- database/database.sh
+++ database/database.sh
@@ -2,8 +2,8 @@
 
 export NEW_HASURA_URL=$1 NEW_HASURA_SECRET=$2 NEW_DB=$3
 
-python3 graphql_setup.py
+python graphql_setup.py
 
-python3 migration_data_loading/load_iso_codes.py
-python3 migration_data_loading/load_bible_locations.py
-python3 migration_data_loading/load_questions_answers.py
+python migration_data_loading/load_iso_codes.py
+python migration_data_loading/load_bible_locations.py
+python migration_data_loading/load_questions_answers.py
