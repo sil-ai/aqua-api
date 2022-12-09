10aa284cda26c1763a0a24739b781c506e6affa6
Mark <woodwardmw@gmail.com>
2022-12-08 16:45:04 -0800

    Add prints for debugging

--- pipelines/scripts/word_alignment/create_refs.py
+++ pipelines/scripts/word_alignment/create_refs.py
@@ -87,6 +87,9 @@ def main(args):
         source = get_source_txt(base_inpath, source_str, all_references)
         if source == None:
             continue
+        files = (base_outpath / f'{source_str}').glob('*')
+        for file in files:
+            print(file.name)
         if ((base_outpath / f'{source_str}/{source_str}_all_ref_verse_scores.csv').exists()
             and (base_outpath / f'{source_str}/{source_str}_all_ref_word_scores.csv').exists()
             and (base_outpath / f'{source_str}/{source_str}_ref_verse_scores.csv').exists()
