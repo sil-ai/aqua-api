095d7eefe2d2fa30a9a8deef15ddecfe187c3304
Mark <woodwardmw@gmail.com>
2022-12-13 22:16:24 -0800

    Delete src and txt files after use

--- pipelines/scripts/alignment_scores/alignment_scores.py
+++ pipelines/scripts/alignment_scores/alignment_scores.py
@@ -48,9 +48,6 @@ def get_best_alignment_scores(
         word_pairs = alignment.to_aligned_word_pairs()
         model.compute_aligned_word_pair_scores(source_segment, target_segment, word_pairs)
 
-        verse_score = model.get_avg_translation_score(
-            source_segment, target_segment, alignment
-        )
         vref = vrefs[c] if vrefs else None
         c = c + 1
         for pair in word_pairs:
@@ -58,7 +55,6 @@ def get_best_alignment_scores(
             data["target"].append(target_segment[pair.target_index])
             data["alignment_count"] = 1
             data["alignment_score"].append(pair.alignment_score)
-            data["verse_score"].append(verse_score)
             data["vref"].append(vref)
 
     df = pd.DataFrame(data)
@@ -81,7 +77,7 @@ def get_vref_scores(df: pd.DataFrame) -> pd.DataFrame:
     vref_df = df[["vref", "verse_score"]]
     return vref_df
 
-def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+def average_scores(df: pd.DataFrame) -> pd.DataFrame:
     """
     Takes a dataframe of aligned matches, removes duplicate source word / target word combinations,
     returns the dataframe.
@@ -94,7 +90,7 @@ def remove_duplicates(df: pd.DataFrame) -> pd.DataFrame:
     """
     # remove duplicates and average out verse and word scores
     dups = df.groupby(["source", "target"]).size().reset_index()
-    avgs = df[['source', 'target', 'alignment_count', 'verse_score', 'alignment_score']].groupby(["source", "target"]).mean(numeric_only=True).reset_index()
+    avgs = df[['source', 'target', 'alignment_count', 'alignment_score']].groupby(["source", "target"]).mean(numeric_only=True).reset_index()
     no_dups = pd.merge(dups, avgs)
     no_dups.drop(columns=['alignment_count'], inplace=True)
     no_dups.rename(columns={0: "alignment_count"}, inplace=True)
@@ -148,15 +144,15 @@ def run_best_align(
     df = get_best_alignment_scores(symmetrized_model, parallel_corpus, vrefs)
 
     # Remove duplicates
-    no_dups = remove_duplicates(df)
+    avg_df = average_scores(df)
 
     # write results to csv
     if not outpath.exists():
         outpath.mkdir(parents=True)
 
-    no_dups.to_csv(outpath / "avg_alignment_scores.csv", index=False)
+    avg_df.to_csv(outpath / "avg_alignment_scores.csv", index=False)
 
-    df.to_csv(outpath / "alignment_scores_by_verse.csv", index=False)
+    df.to_csv(outpath / "alignment_scores.csv", index=False)
 
     # delete temp files
     condensed_source.unlink()
@@ -200,6 +196,10 @@ def main(args):
             target = target_dir / f'{target_str}.txt'
             outpath = base_outpath / f'{source_str}_{target_str}/'
             run_best_align(source, target, outpath, is_bible=args.is_bible)
+            meta = {'source': source_str, 'target': target_str}
+            with open(outpath / 'meta.json', 'w') as f:
+                json.dump(meta, f)
+
 
 
 if __name__ == "__main__":
