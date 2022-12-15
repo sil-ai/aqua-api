291fe0c6f61284508de1f445041e9b312155d1e2
Mark <woodwardmw@gmail.com>
2022-12-15 11:52:04 -0800

    Remove drop for columns that aren't there

--- pipelines/scripts/total_scores/total_scores.py
+++ pipelines/scripts/total_scores/total_scores.py
@@ -64,10 +64,8 @@ def main(args):
     all_results = alignment_scores.merge(avg_alignment_scores, how='left', on=['source', 'target'])
     all_results = all_results.merge(translation_scores, how='left', on=['source', 'target'])
     all_results.loc[:, ['avg_aligned']] = all_results.apply(lambda row: row['alignment_count'] / row['co-occurrence_count'], axis = 1).astype('float16')
-    all_results = all_results.drop(['alignment_count', 'co-occurence_count'], axis=1)
     all_results.loc[:, 'translation_score'] = all_results.loc[:, 'translation_score'].apply(lambda x: 0 if x < 0.00001 else x).astype('float16')
     all_results.loc[:, "match_score"] = get_data.faster_df_apply(all_results, lambda x: get_scores_from_match_dict(match_scores, x["source"], x["target"], normalized=False)[0]).astype('float16')
-    # all_results.loc[:, "match_counts"] = get_data.faster_df_apply(all_results, lambda x: get_scores_from_match_dict(match_scores, x["source"], x["target"], normalized=False)[1]).astype('int')
     all_results.loc[:, 'total_score'] = get_data.faster_df_apply(all_results,lambda row: (row['avg_aligned'] + row['translation_score'] + row['alignment_score'] + row['match_score'] + row['embedding_score']) / 5)
     
     total_scores = all_results[['vref', 'source', 'target', 'total_score']]
