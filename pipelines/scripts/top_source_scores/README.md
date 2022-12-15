3e2935d3638853f110d6d7399a10eb8998681a25
Mark <woodwardmw@gmail.com>
2022-12-14 16:02:59 -0800

    Initial commit

---
+++ pipelines/scripts/top_source_scores/README.md
@@ -0,0 +1,5 @@
+# Top Source Scores
+
+This pipeline takes the output from the [Total Scores](../total_scores/) pipeline and filters to only keep the highest score from each vref - source word combination.
+
+These scores are outputed to `top_source_scores.csv`.
---
+++ pipelines/scripts/top_source_scores/top_source_scores.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import json
+import argparse
+
+import pandas as pd
+
+
+def remove_leading_and_trailing_blanks(df:pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Takes a dataframe and removes all rows before the first non-blank entry in a column, and after the last non-blank entry.
+    """
+    df = df[(df.loc[:, col].notna().cumsum() > 0) & (df.loc[::-1, col].notna().cumsum() > 0)]
+    return df
+
+def get_verse_scores(total_scores: pd.DataFrame):
+    top_source_scores = remove_leading_and_trailing_blanks(total_scores, 'total_score')
+    top_source_scores = top_source_scores.fillna(0)
+    top_source_scores = top_source_scores.loc[top_source_scores.groupby(['vref', 'source'], sort=False)['total_score'].idxmax(), :].reset_index(drop=True)
+    return top_source_scores
+    
+    
+def main(args):
+    for total_scores_dir in args.total_scores_dir.iterdir():
+        print(total_scores_dir)
+        meta_file = total_scores_dir / 'meta.json'
+        with open(meta_file) as f:
+            meta = json.load(f)
+        source_str = meta['source']
+        target_str = meta['target']
+        outpath = args.outpath / f'{source_str}_{target_str}'
+        total_scores = pd.read_csv(total_scores_dir / 'total_scores.csv')
+        top_source_scores = get_verse_scores(total_scores)
+        top_source_scores.to_csv(outpath / 'top_source_scores.csv')
+    with open(args.outpath / 'meta.json', 'w') as f:
+                json.dump(meta, f)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--total-scores-dir", type=Path, help="directory with total scores")
+    parser.add_argument("--outpath", type=Path, default=Path("/pfs/out"), help="Output directory")
+    args = parser.parse_args()
+
+    main(args) 
