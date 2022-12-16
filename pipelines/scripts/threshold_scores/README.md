5fcb4d1855bb881fd5046aa654267012559ed382
Mark <woodwardmw@gmail.com>
2022-12-15 21:40:41 -0800

    Initial commit

---
+++ pipelines/scripts/threshold_scores/README.md
@@ -0,0 +1,5 @@
+# Threshold Scores
+
+This pipeline takes the output from the [Total Scores](../total_scores/) pipeline and filters to only keep those scores over a threshold. This is the used for Word Alignment visualizations.
+
+These scores are outputed to `threshold_scores.csv`.
---
+++ pipelines/scripts/threshold_scores/threshold_scores.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import json
+import argparse
+
+import pandas as pd
+
+
+def remove_leading_and_trailing_blanks(df:pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Takes a dataframe and removes all rows before the first non-blank entry in a column, and after the last non-blank entry.
+    """
+    df = df[(df.loc[:, col].notna().cumsum() > 0) & (df.loc[::-1, col].notna().cumsum() > 0)]
+    return df
+
+def get_threshold_scores(total_scores: pd.DataFrame, threshold: float):
+    top_source_scores = remove_leading_and_trailing_blanks(total_scores, 'total_score')
+    top_source_scores = top_source_scores.fillna(0)
+    top_source_scores = top_source_scores.loc[top_source_scores['total_score'] > threshold]
+    return top_source_scores
+    
+    
+def main(args):
+    for dir in args.total_scores_dir.iterdir():
+        print(dir)
+        meta_file = dir / 'meta.json'
+        with open(meta_file) as f:
+            meta = json.load(f)
+        source_str = meta['source']
+        target_str = meta['target']
+        outpath = args.outpath / f'{source_str}_{target_str}'
+        if not outpath.exists():
+            outpath.mkdir()
+        total_scores = pd.read_csv(dir / 'total_scores.csv')
+        top_source_scores = get_threshold_scores(total_scores, args.threshold)
+        top_source_scores.to_csv(outpath / 'threshold_scores.csv')
+        with open(outpath / 'meta.json', 'w') as f:
+            json.dump(meta, f)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--total-scores-dir", type=Path, help="directory with total scores")
+    parser.add_argument("--outpath", type=Path, default=Path("/pfs/out"), help="Output directory")
+    parser.add_argument("--threshold", type=float, default=Path("/pfs/out"), help="Threshold for keeping scores")
+
+    args = parser.parse_args()
+
+    main(args) 
