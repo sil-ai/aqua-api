a74826db64382448b6a8a8695c4315873f879c90
LOHMgit <lightofhopemedia@gmail.com>
2022-12-30 12:58:48 +0400

    assessments are updating through orm

--- pipelines/scripts/switchboard/models.py
+++ pipelines/scripts/switchboard/models.py
@@ -1,19 +1,46 @@
-from sqlalchemy import Column, Integer, Text, Boolean
+import enum
+from sqlalchemy import Column, Integer, Text,\
+                       ForeignKey, DateTime, Enum, VARCHAR
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
+
+class VerseText(Base):
+
+    __tablename__ = "verseText"
+    id = Column(Integer, primary_key=True)
+    text = Column(Text)
+    bibleRevision = Column(Integer)
+    verseReference = Column(Text)
+
+    def __repr__(self):
+        return (
+            f"VerseText({self.id} - Revision {self.bibleRevision})\n"
+            f"{self.text} - {self.verseReference}"
+        )
+
+class StatusEnum(enum.Enum):
+    #TODO: maybe change this
+    RUNNING = 'running'
+    QUEUED = 'queued'
+    FAILED = 'failed'
+    COMPLETE = 'complete'
 
 class Assessment(Base):
 
     __tablename__ = "assessment"
-    id = Column(Integer, primary_key=True)
-    revision = Column(Integer)
-    reference = Column(Integer)
-    finished = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True)#autoincrements by default
+    revision = Column(Integer, ForeignKey(VerseText.bibleRevision))
+    reference = Column(Integer, ForeignKey(VerseText.bibleRevision))
     type = Column(Text)
+    start_time = Column(DateTime, default=datetime.now())
+    end_time = Column(DateTime)
+    status = Column(Enum(StatusEnum), nullable=False)
+    job_id = Column(VARCHAR, required=True)
 
     def __repr__(self):
         return (
             f"Assessment({self.id}) - {self.type} "
-            f"revision={self.revision} reference={self.reference}, finished={self.finished}"
+            f"revision={self.revision} reference={self.reference}, status={self.status}"
         )
