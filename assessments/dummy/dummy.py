984b3b9b053e5f09e8f8370e6943d58455a78a67
Mark <woodwardmw@gmail.com>
2023-01-13 16:54:41 -0800

    Longer sleep time

--- assessments/dummy/dummy.py
+++ assessments/dummy/dummy.py
@@ -8,6 +8,6 @@ stub = modal.Stub(name="dummy", image=modal.Image.debian_slim())
 
 
 @stub.function(timeout=sleep_time + 300)
-def dummy(config_details: dict):
+def dummy(configuration: dict):
     time.sleep(sleep_time)
-    return (200, config_details)
+    return (200, configuration)
