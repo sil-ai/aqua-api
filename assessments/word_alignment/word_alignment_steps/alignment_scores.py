97cf731f07cf6bd7d8ae99566dec7105b56f55eb
Mark <woodwardmw@gmail.com>
2023-01-18 11:49:16 -0800

    Changed AssessmentConfig.assessment_type to enum instead of string

---
+++ assessments/word_alignment/word_alignment_steps/alignment_scores.py
@@ -0,0 +1,227 @@
+# imports
+import argparse
+from typing import List, Tuple, Optional
+import json
+import sys
+from pathlib import Path
+
+import pandas as pd
+from machine.corpora import TextFileTextCorpus
+from machine.tokenization import LatinWordTokenizer
+from machine.translation import SymmetrizationHeuristic
+sys.path.append((Path(__file__).parent.parent).as_posix())
+import common_files.get_data as get_data
+import common_files.train_fa_model as train_fa_model
+
+from machine.translation.thot import (
+    ThotFastAlignWordAlignmentModel,
+    ThotSymmetrizedWordAlignmentModel,
+)
+from pathlib import Path
+
+
+def get_best_alignment_scores(
+                                model: ThotSymmetrizedWordAlignmentModel, 
+                                corpus: TextFileTextCorpus, 
+                                vrefs: Optional[List[str]] = None
+                                ) -> pd.DataFrame:
+    """
+    Takes a corpus and a word alignment model and calculates word alignments for each aligned line.
+    Returns a dataframe with words that have been aligned by the model.
+
+    Inputs:
+    model           A machine.translation.thot.ThotSymmetrizedWordAlignmentModel
+    corpus          A machine.corpora.TextFileTextCorpus of aligned texts
+    vrefs           An optional list of verse references for each aligned line
+
+    Outputs:
+    df                      A dataframe, where each row is an alignment of a source word and a target word.
+        source              The source word
+        target              The target word
+        alignment_count     Integer 1, to later be summed as a count
+        verse_score         The average alignment score for the line in question
+        vref                The verse reference for that line,     if a vref file has been supplied.
+    """
+    data = {"vref": [], "source": [], "target": [], "alignment_count": [], "alignment_score": []}
+    segments = list(corpus.lowercase().to_tuples())
+    alignments = model.align_batch(segments)
+    c = 0
+    # for source_segments, target_segments in batch(segments, model.batch_size):
+
+    for (source_segment, target_segment), alignment in zip(list(segments), alignments):
+        word_pairs = alignment.to_aligned_word_pairs()
+        model.compute_aligned_word_pair_scores(source_segment, target_segment, word_pairs)
+
+        vref = vrefs[c] if vrefs else None
+        c = c + 1
+        for pair in word_pairs:
+            data["source"].append(source_segment[pair.source_index])
+            data["target"].append(target_segment[pair.target_index])
+            data["alignment_count"] = 1
+            data["alignment_score"].append(pair.alignment_score)
+            data["vref"].append(vref)
+
+    df = pd.DataFrame(data)
+    return df
+
+
+def average_scores(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Takes a dataframe of aligned matches, removes duplicate source word / target word combinations,
+    returns the dataframe.
+
+    Inputs:
+    df          A dataframe of alignment matches
+
+    Outputs:
+    no_dups     A dataframe with duplicates removed, grouped by source and target words.
+    """
+    # remove duplicates and average out verse and word scores
+    dups = df.groupby(["source", "target"]).size().reset_index()
+    avgs = df[['source', 'target', 'alignment_count', 'alignment_score']].groupby(["source", "target"]).mean(numeric_only=True).reset_index()
+    no_dups = pd.merge(dups, avgs)
+    no_dups.drop(columns=['alignment_count'], inplace=True)
+    no_dups.rename(columns={0: "alignment_count"}, inplace=True)
+    return no_dups
+
+
+def save_alignment_scores(df, filename: Path):
+    print(filename)
+    df[df.select_dtypes(['float']).columns] = df.select_dtypes(['float']).astype('float16')
+    (filename.parent).mkdir(parents=True, exist_ok=True)
+    df.to_csv(filename, index=False)
+
+
+def run_best_align(
+                    source: Path, 
+                    target: Path, 
+                    outpath: Path, 
+                    is_bible: bool=False, 
+                    parallel_corpus: TextFileTextCorpus = None, 
+                    symmetrized_model: ThotSymmetrizedWordAlignmentModel = None,
+                    ) -> Tuple[TextFileTextCorpus, ThotSymmetrizedWordAlignmentModel]:
+    """
+    Takes two input text files, runs get_alignments on them, and saves the resulting dataframe
+    to a csv file in a directory within outpath.
+
+    Inputs:
+    source           Path to a source text file of line-aligned text
+    target           Path to a target text file of line-aligned text
+    outpath            Path to base output directory
+    is_bible           Boolean for whether the text is Bible, and hence vref references should be used.
+    parallel_corpus    A corpus to process. Normally the corpus is produced from the source and target,
+                        but if it has already been produced it can optionally be provided here to save
+                        calculating it again.
+    symmetrized_model   The model to use. Normally the model is instantiated and trained with the source and target,
+                        but if it has already been created and trained it can optionally be provided here to save
+                        training it again.
+
+    Outputs:
+    TextFileTextCorpus      In case you want to re-use it without creating it from scratch
+    ThotSymmetrizedWordAlignmentModel       In case you want to re-use it without training from scratch
+    """
+    if not outpath.exists():
+        outpath.mkdir(parents=True)
+    # get vrefs
+    ref_df = get_data.get_ref_df(source, target, is_bible)
+    df = get_data.condense_files(ref_df)
+    vrefs = list(df['vref'])
+    condensed_source, condensed_target = get_data.write_condensed_files(df, outpath)
+    
+    # create parallel corpus
+    if not parallel_corpus:
+        parallel_corpus = get_data.create_corpus(condensed_source, condensed_target)
+
+    # Train fast_align model
+    if not symmetrized_model:
+        symmetrized_model = train_fa_model.train_model(parallel_corpus)
+
+    # Get alignments
+    print("Getting alignment scores...")
+    df = get_best_alignment_scores(symmetrized_model, parallel_corpus, vrefs)
+
+    # Remove duplicates
+    avg_df = average_scores(df)
+    avg_df = avg_df.drop(['alignment_score'], axis=1)
+    df = df.drop(['alignment_count'], axis=1)
+
+    df[df.select_dtypes(['float']).columns] = df.select_dtypes(['float']).astype('float16')
+    avg_df[avg_df.select_dtypes(['float']).columns] = avg_df.select_dtypes(['float']).astype('float16')
+
+    # write results to csv
+    # save_alignment_scores(avg_df, outpath / "avg_alignment_scores.csv")
+    # save_alignment_scores(df, outpath / "alignment_scores.csv")
+
+    # delete temp files
+    condensed_source.unlink()
+    condensed_target.unlink()
+
+    return df, avg_df, parallel_corpus, symmetrized_model
+
+
+def get_source(source_dir: Path):
+    print(source_dir)
+    meta_file = source_dir / 'meta.json'
+    with open(meta_file) as f:
+        meta = json.load(f)
+    source_str = meta['source']
+    source = source_dir / f'{source_str}.txt'
+    return source, source_str
+
+
+def is_source_in_config(source_str, target_str, config_dir):
+    print(target_str)
+
+    config_file = config_dir / f'{target_str}-config.json'
+    if config_file.exists():
+        print("Found config file")
+        with open(config_file) as f:
+            config = json.loads(f.read())
+        requested_sources = config.get('sources', [])
+        is_ref = config.get('ref', False)
+        refresh = config.get('refresh', False)
+        print(f'Is Ref? {is_ref}')
+        print(f'Requested sources: {requested_sources}')
+        if source_str not in requested_sources and not is_ref:
+            print(f"Skipping target {target_str} for source {source_str}")
+            return False
+    print("No config file found, will compare with all sources")
+    return True
+
+
+def main(args):
+    sources = args.source_dir
+    targets = args.target_dir
+    base_outpath = args.outpath
+    config_dir = args.config_dir
+
+    for source_dir in sources.iterdir():
+        source, source_str = get_source(source_dir)
+        for target_dir in targets.iterdir():
+            meta_file = target_dir / 'meta.json'
+            with open(meta_file) as f:
+                meta = json.load(f)
+            target_str = meta['source']
+            if not is_source_in_config(source_str, target_str, config_dir):
+                continue
+
+            target = target_dir / f'{target_str}.txt'
+            outpath = base_outpath / f'{source_str}_{target_str}/'
+            run_best_align(source, target, outpath, is_bible=args.is_bible)
+            meta = {'source': source_str, 'target': target_str}
+            with open(outpath / 'meta.json', 'w') as f:
+                json.dump(meta, f)
+
+
+
+if __name__ == "__main__":
+    # command line args
+    parser = argparse.ArgumentParser(description="Argparser")
+    parser.add_argument("--source-dir", type=Path, help="source bible directory")
+    parser.add_argument("--target-dir", type=Path, help="target bible directory")
+    parser.add_argument("--outpath", type=Path, default=Path("/pfs/out"), help="Output directory")
+    parser.add_argument("--config-dir", type=Path, help="Path to config dir", required=True)
+    parser.add_argument("--is-bible", action="store_true", help="Whether text is Bible, in which case the length of the text file must be 41,899 lines")
+    args, unknown = parser.parse_known_args()
+    main(args)
+
--- runner/runner/runner.py
+++ runner/runner/runner.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 import os
 import json
+from enum import Enum
 
 import modal
 from fastapi import UploadFile, BackgroundTasks
@@ -11,26 +12,28 @@ if os.environ.get("MODAL_TEST") == "TRUE":
     suffix = "_test"
 
 
-stub = modal.Stub(
-    name="runner" + suffix,
-    image=modal.Image.debian_slim().pip_install()
-)
-assessment_types = [
-    "dummy",  # List all assessment types here
-]
-for assessment_type in assessment_types:
-    stub[assessment_type] = modal.Function.from_name(assessment_type, assessment_type)
+stub = modal.Stub(name="runner" + suffix, image=modal.Image.debian_slim().pip_install())
+
+
+class AssessmentType(Enum):
+    dummy = 1
+
+
+for assessment_type in AssessmentType:
+    stub[assessment_type.name] = modal.Function.from_name(
+        assessment_type.name, assessment_type.name
+    )
 
 
 class AssessmentConfig(BaseModel):
     assessment: int
-    assessment_type: str  # Or Enum?
+    assessment_type: AssessmentType
     configuration: dict  # This will later be validated as a BaseModel by the specific assessment
 
 
 @stub.function(timeout=7200)
 def run_assessment_runner(assessment_config: AssessmentConfig):
-    return modal.container_app[assessment_config.assessment_type].call(
+    return modal.container_app[assessment_config.assessment_type.name].call(
         assessment_config.configuration
     )
 
@@ -39,11 +42,14 @@ def run_assessment_runner(assessment_config: AssessmentConfig):
 async def assessment_runner(file: UploadFile, background_tasks: BackgroundTasks):
     config_file = await file.read()
     config = json.loads(config_file)
+    if config["assessment_type"] not in [e.name for e in AssessmentType]:
+        raise ValueError(f"Invalid assessment type: {config['assessment_type']}")
+    config["assessment_type"] = AssessmentType[config["assessment_type"]]
     assessment_config = AssessmentConfig(**config)
     background_tasks.add_task(run_assessment_runner, assessment_config)
     return {
-        'status_code': 200,
-        'content': "Assessment runner started in the background, will take approximately 20 minutes to finish.",
+        "status_code": 200,
+        "content": "Assessment runner started in the background, will take approximately 20 minutes to finish.",
     }
 
 
