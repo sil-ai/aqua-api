7a913f5ceb069ab84eb9a934719a19f3a78b498f
cassiep429 <cassandra.powell@mail.mcgill.ca>
2023-01-19 15:09:23 -0500

    Update test

--- assessments/sentence-length/assess_test.py
+++ assessments/sentence-length/assess_test.py
@@ -1,6 +1,18 @@
 import modal
 from assess import SentLengthAssessment, SentLengthConfig
 import pandas as pd
+import requests
+from pathlib import Path
+
+def test_runner():
+    url = "https://sil-ai--runner-test-assessment-runner.modal.run/"
+
+    config_filepath = Path('fixtures/test_config.json')
+
+    with open(config_filepath) as json_file:
+        response = requests.post(url, files={"file": json_file})
+
+    assert response.status_code == 200
 
 stub = modal.Stub(
     name="assess_test",
