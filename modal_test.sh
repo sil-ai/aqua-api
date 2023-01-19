bac31014d78600fb6f1dda024014b29d6fc9a3b3
Daniel Whitenack <whitenack.daniel@gmail.com>
2023-01-19 13:57:00 -0500

    repo wide modal testing

---
+++ modal_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install Modal
+pip install modal-client pytest
+
+# Authenticate Modal
+modal token set --token-id $MODAL_TOKEN_ID --token-secret $MODAL_TOKEN_SECRET --env=sil-ai
+
+# Current working directory.
+currentdir=$(pwd)
+
+# Deploy dummy assessment endpoint (in case it was updated)
+modal deploy assessments/dummy/dummy.py || exit 1
+
+# Test all Modal apps.
+for dir in runner assessments
+do 
+for app in $dir*/
+do
+echo ""
+echo ""
+echo "Testing Modal app: $app"
+echo "-----------------------"
+    cd $app
+    MODAL_TEST=TRUE modal deploy app.py || exit 1
+    pytest || exit 1
+    cd $currentdir
+done
+done
