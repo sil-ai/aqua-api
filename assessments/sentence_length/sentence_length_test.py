56f2968009357ff41843af01d123ad81a2018635
cassiep429 <cassandra.powell@mail.mcgill.ca>
2023-01-19 15:35:53 -0500

    Update test assessment_id

--- assessments/sentence_length/sentence_length_test.py
+++ assessments/sentence_length/sentence_length_test.py
@@ -35,7 +35,7 @@ def test_assess_draft_10():
     with stub.run():
         # Initialize some SentLengthAssessment value.
         config = SentLengthConfig(draft_revision=10)
-        assessment = SentLengthAssessment(assessment_id=1, assessment_type='sentence_length', configuration=config)
+        assessment = SentLengthAssessment(assessment_id=2, assessment_type='sentence_length', configuration=config)
         results = get_results.call(assessment)
 
         #assert the length of results is 41899
@@ -50,7 +50,7 @@ def test_assess_draft_11():
     with stub.run():
         # Initialize some SentLengthAssessment value.
         config = SentLengthConfig(draft_revision=11)
-        assessment = SentLengthAssessment(assessment_id=1, assessment_type='sentence_length', configuration=config)
+        assessment = SentLengthAssessment(assessment_id=2, assessment_type='sentence_length', configuration=config)
         results = get_results.call(assessment)
 
     #assert the length of results is 41899
