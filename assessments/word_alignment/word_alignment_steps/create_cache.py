62f8bfa58841f47ba982c316d47aa962acff30fc
Mark <woodwardmw@gmail.com>
2023-01-24 22:46:30 -0800

    Added src_list to tokenized_df and module name to normalize_words

--- assessments/word_alignment/word_alignment_steps/create_cache.py
+++ assessments/word_alignment/word_alignment_steps/create_cache.py
@@ -11,6 +11,7 @@ import word_alignment_steps.prepare_data as prepare_data
 
 
 def create_index_cache(tokenized_df):
+    tokenized_df.loc[:, 'src_list'] = tokenized_df['src_tokenized'].apply(lambda x: str(x).split())
     word_dict = get_indices_from_df(tokenized_df)
     index_cache = {key: value.index_list for key, value in word_dict.items()}
     return index_cache
@@ -31,7 +32,7 @@ def get_indices_from_df(tokenized_df: pd.DataFrame) -> Dict[str, prepare_data.Wo
     all_source_words = list(tokenized_df['src_list'].explode().unique())
     word_dict = {word: prepare_data.Word(word) for word in all_source_words if type(word) == str}
 
-    word_series = tokenized_df['src_list'].explode().apply(lambda x: normalize_word(x))
+    word_series = tokenized_df['src_list'].explode().apply(lambda x: prepare_data.normalize_word(x))
     for word in word_dict.values():
         word.get_indices(word_series)
     return word_dict
