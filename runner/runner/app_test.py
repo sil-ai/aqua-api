21d4a18185a2d72740f4689257d3fa46a0836504
DCNemesis <3616964+DCNemesis@users.noreply.github.com>
2023-01-25 11:00:07 -0500

    add auth to app_test

--- runner/runner/app_test.py
+++ runner/runner/app_test.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+import os
 
 
 def test_runner():
@@ -8,7 +9,11 @@ def test_runner():
     config_filepath = Path('fixtures/test_config.json')
 
     with open(config_filepath) as json_file:
-        response = requests.post(url, files={"file": json_file})
+        response = requests.post(
+            url, 
+            files={"file": json_file},
+            headers={'Authorization':f'access_token {os.getenv("TEST_KEY")}'},
+            )
 
     assert response.status_code == 200
 
