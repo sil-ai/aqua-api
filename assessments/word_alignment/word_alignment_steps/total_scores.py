8e902ad57264c28fb7eb35f0b89dcc665c8356ac
Mark <woodwardmw@gmail.com>
2023-01-24 22:48:29 -0800

    Added split for source and target strings. Merge match_scores_df.

--- assessments/word_alignment/word_alignment_steps/total_scores.py
+++ assessments/word_alignment/word_alignment_steps/total_scores.py
@@ -26,20 +26,26 @@ def run_total_scores(
                 match_scores_df: pd.DataFrame,
                 embedding_scores_df: pd.DataFrame,
                 ):
+    condensed_df.loc[:, 'source'] = condensed_df['src'].apply(lambda x: str(x).split())
+    condensed_df.loc[:, 'target'] = condensed_df['trg'].apply(lambda x: str(x).split())
+    condensed_df = condensed_df.explode('source').explode('target')
     alignment_scores_df['vref'] = alignment_scores_df['vref'].astype('object')  # Necessary for non-Bible, where vrefs are ints.
-    alignment_scores_df = alignment_scores_df.merge(avg_alignment_scores_df, how = 'left', on=['source', 'target']).fillna(0)
 
+    alignment_scores_df = alignment_scores_df.merge(avg_alignment_scores_df, how = 'left', on=['source', 'target']).fillna(0)
     all_results = condensed_df.merge(alignment_scores_df, how='left', on=['vref', 'source', 'target']).fillna(0)
+
     all_results = all_results.merge(translation_scores_df, how='left', on=['source', 'target'])
+
     all_results.loc[:, 'avg_aligned'] = all_results.apply(lambda row: row['alignment_count'] / row['co-occurrence_count'], axis = 1).astype('float16')
     all_results.loc[:, 'translation_score'] = all_results.loc[:, 'translation_score'].apply(lambda x: 0 if x < 0.00001 else x).astype('float16')
-    all_results.loc[:, 'match_score'] = all_results.merge(match_scores_df, how='left', on=['source', 'target'])
-    # all_results.loc[:, "match_score"] = faster_df_apply(all_results, lambda x: get_scores_from_match_dict(match_scores_df, x["source"], x["target"], normalized=False)[0]).astype('float16')
+
+    all_results = all_results.merge(match_scores_df, how='left', on=['source', 'target'])
+
     all_results = all_results.merge(embedding_scores_df, how='left', on=['source', 'target'])
+
     all_results.loc[:, 'total_score'] = faster_df_apply(all_results,lambda row: (row['avg_aligned'] + row['translation_score'] + row['alignment_score'] + row['match_score'] + row['embedding_score']) / 5)
     
     total_scores_df = all_results[['vref', 'source', 'target', 'total_score']]
-
     top_source_scores_df = total_scores_df.fillna(0)
     top_source_scores_df = top_source_scores_df.loc[top_source_scores_df.groupby(['vref', 'source'], sort=False)['total_score'].idxmax(), :].reset_index(drop=True)
 
