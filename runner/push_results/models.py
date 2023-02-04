d09d86a51415067e2f0c3e958bdf649b1e32d9a6
Mark <woodwardmw@gmail.com>
2023-02-03 23:19:06 -0800

    Add MissingWord and MissingWords

--- runner/push_results/models.py
+++ runner/push_results/models.py
@@ -10,7 +10,20 @@ class Result(BaseModel):
     flag: bool = False
     note: Optional[str] = None
 
+# Results model to record in the DB.
+class MissingWord(BaseModel):
+    assessment_id: int
+    vref: str
+    source: str
+    score: float
+    flag: bool = False
+    note: Optional[str] = None
+
 
 # Results is a list of results to push to the DB
 class Results(BaseModel):
     results: List[Result]
+
+# Results is a list of missing words to push to the DB
+class MissingWords(BaseModel):
+    missing_words: List[MissingWord]
