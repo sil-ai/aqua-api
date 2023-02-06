11d630aa8db485aff5ece2ed73062c2048324fc5
lohm <lightofhopemedia@gmail.com>
2023-02-06 08:59:34 +0400

    added test_swahili

--- assessments/semantic-similarity/conftest.py
+++ assessments/semantic-similarity/conftest.py
@@ -1,13 +1,22 @@
 import pytest
+import pickle
 from app import SemanticSimilarity
 
 @pytest.fixture(scope='session')
-def model():
+def model(semsim):
     return SemanticSimilarity().semsim_model
 
 @pytest.fixture(scope='session')
 def tokenizer():
     return SemanticSimilarity().semsim_tokenizer
+
+@pytest.fixture(scope='session')
+def revisions():
+    return pickle.load(open('./fixtures/revisions_feb_4.pkl','rb'))
+
+@pytest.fixture(scope='session')
+def swahili_revision(revisions):
+    return revisions[1]
 
 class ValueStorage:
     results = None
--- assessments/semantic-similarity/test_semsim.py
+++ assessments/semantic-similarity/test_semsim.py
@@ -1,6 +1,9 @@
 import modal
 import pytest
+import random
+from string import ascii_letters
 from semsim_models import SemSimConfig, SemSimAssessment
+from app import SemanticSimilarity
 
 stub = modal.Stub(
     name="semantic_similarity",
@@ -18,10 +21,15 @@ stub = modal.Stub(
 )
 
 stub.assess = modal.Function.from_name("semantic_similarity", "assess")
+stub.predict = modal.Function.from_name("semantic_similarity","predict")
 
 @stub.function
 def get_assessment(ss_assessment: SemSimAssessment, offset: int=-1):
     return modal.container_app.assess.call(ss_assessment, offset)
+
+@stub.function
+def get_prediction(sent1: str, sent2: str, assessment_id: int=1):
+    return modal.container_app.predict.call(sent1, sent2, assessment_id)
 
 #tests the assessment object
 @pytest.mark.parametrize(
@@ -39,7 +47,7 @@ def test_assessment_object(draft_id, ref_id, expected, valuestorage):
                               type="semantic-similarity")
         assessment = SemSimAssessment(assessment_id=1, configuration=config)
         print(assessment)
-        results = get_assessment.call(assessment, offset=105)
+        results = get_assessment.call(assessment)
         print(results)
         #test for the right type of results
         assert type(results) == Results
@@ -71,3 +79,31 @@ def test_predictions(idx, expected, valuestorage):
         assert int(round(score,0)) == expected
     except TypeError:
         raise ValueError('No result values')
+
+def create_draft_verse(verse, variance):
+    num_of_chars = int(round(variance/100*len(verse),0))
+    idx = [i for i,_ in enumerate(verse) if not verse.isspace()]
+    sam = random.sample(idx, num_of_chars)
+    lst = list(verse)
+    for ind in sam:
+        lst[ind] = random.choice(ascii_letters)
+    return "".join(lst)
+
+@pytest.mark.parametrize('verse_offset, variance',
+                            [
+                                (42,10),
+                                (1042,20),
+                                (4242,30),
+                            ],
+                            ids=['GEN 26:21','GEN 32:22','Num 16:9']
+                        )
+def test_swahili_revision(verse_offset, variance, swahili_revision):
+    with stub.run():
+        #TODO: almost there!?
+        from fuzzywuzzy import fuzz
+        semsim = SemanticSimilarity()
+        verse = swahili_revision.iloc[verse_offset]['text']
+        draft_verse = create_draft_verse(verse, variance)
+        print(verse, draft_verse, fuzz.ratio(verse,draft_verse))
+        prediction = get_assessment.call(verse, draft_verse, id,1)
+        print(prediction)
