ce9ddbe6ea835cbbb0b3a0c76ea6ac17bb64eba1
Mark <woodwardmw@gmail.com>
2023-02-07 21:41:07 -0800

    Add base_url and header

--- assessments/semantic-similarity/conftest.py
+++ assessments/semantic-similarity/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 class ValueStorage:
     results = None
@@ -6,3 +7,13 @@ class ValueStorage:
 @pytest.fixture(scope='session')
 def valuestorage():
     return ValueStorage()
+
+
+@pytest.fixture
+def base_url():
+    return os.getenv("AQUA_URL")
+
+@pytest.fixture
+def header():
+    key =  "Bearer" + " " + str(os.getenv("TEST_KEY"))
+    return {"Authorization": key}
