cd74a7b25e32dce6076dee98938860f927e8d17c
Mark <woodwardmw@gmail.com>
2023-02-14 12:25:20 -0800

    Add print statements for progress, make all_results smaller by dropping columns and groupby

--- assessments/word_alignment/word_alignment_steps/total_scores.py
+++ assessments/word_alignment/word_alignment_steps/total_scores.py
@@ -43,31 +43,35 @@ def run_total_scores(
     """
     condensed_df.loc[:, 'source'] = condensed_df['src'].apply(lambda x: str(x).split())
     condensed_df.loc[:, 'target'] = condensed_df['trg'].apply(lambda x: str(x).split())
+    condensed_df = condensed_df.drop(['src', 'trg'], axis=1)
     condensed_df = condensed_df.explode('source').explode('target')
     alignment_scores_df['vref'] = alignment_scores_df['vref'].astype('object')  # Necessary for non-Bible, where vrefs are ints.
 
     alignment_scores_df = alignment_scores_df.merge(avg_alignment_scores_df, how = 'left', on=['source', 'target']).fillna(0)
+    print("Merging alignment scores")
     all_results = condensed_df.merge(alignment_scores_df, how='left', on=['vref', 'source', 'target']).fillna(0)
-
+    print("Merging translation scores")
     all_results = all_results.merge(translation_scores_df, how='left', on=['source', 'target'])
 
     all_results.loc[:, 'avg_aligned'] = all_results.apply(lambda row: row['alignment_count'] / row['co-occurrence_count'], axis = 1).astype('float16')
     all_results.loc[:, 'translation_score'] = all_results.loc[:, 'translation_score'].apply(lambda x: 0 if x < 0.00001 else x).astype('float16')
     all_results.loc[:, 'source_norm'] = all_results['source'].apply(normalize_word)
     all_results.loc[:, 'target_norm'] = all_results['target'].apply(normalize_word)
+    
     match_scores_df = match_scores_df.rename(columns={'source': 'source_norm', 'target': 'target_norm'})
+    print("Merging match scores")
     all_results = all_results.merge(match_scores_df, how='left', on=['source_norm', 'target_norm'])
-
+    print("Merging embedding scores")
     all_results = all_results.merge(embedding_scores_df, how='left', on=['source', 'target'])
 
-    # Embedding scores currently only work for New Testament
-    all_results.loc[23213:, 'total_score'] = faster_df_apply(all_results.loc[23213:],lambda row: (row['avg_aligned'] + row['translation_score'] + row['alignment_score'] + row['match_score'] + row['embedding_score']) / 5)
-    all_results.loc[:23213, 'total_score'] = faster_df_apply(all_results.loc[:23213],lambda row: (row['avg_aligned'] + row['translation_score'] + row['alignment_score'] + row['match_score'] / 4))
+    print("Calculating total scores")
+    all_results = all_results.groupby(['vref', 'source', 'target'], sort=False, as_index=False).mean()
+    all_results.loc[:, 'total_score'] = faster_df_apply(all_results,lambda row: (row['avg_aligned'] + row['translation_score'] + row['alignment_score'] + row['match_score'] + row['embedding_score']) / 5)
     
     total_scores_df = all_results[['vref', 'source', 'target', 'total_score']]
     top_source_scores_df = total_scores_df.fillna(0)
     top_source_scores_df = top_source_scores_df.loc[top_source_scores_df.groupby(['vref', 'source'], sort=False)['total_score'].idxmax(), :].reset_index(drop=True)
-
+    print("Calculating verse scores")
     verse_scores_df = top_source_scores_df.groupby('vref', as_index=False, sort=False).mean()
     verse_scores_df = verse_scores_df.fillna(0)
 
