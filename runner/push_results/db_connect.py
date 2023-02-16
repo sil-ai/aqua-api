892bb83d2661800c708b9173cfaa4bd91265f84b
Mark <woodwardmw@gmail.com>
2023-02-16 13:23:15 -0800

    Get DB, URL and API secrets from API via runner, not from Modal secrets

--- runner/push_results/db_connect.py
+++ runner/push_results/db_connect.py
@@ -1,14 +1,14 @@
 import os
 
 
-def get_session():
+def get_session(AQUA_DB: str):
     from sqlalchemy.orm import Session
     from sqlalchemy import create_engine
 
     # ??? Should this yield only session?
     # Need engine in testing but maybe I can pull out of session?
     try:
-        engine = create_engine(os.environ["AQUA_DB"], pool_size=5, pool_recycle=3600)
+        engine = create_engine(AQUA_DB, pool_size=5, pool_recycle=3600)
     except KeyError as err:
         raise KeyError(f"Missing environmental variable {err}") from err
     with Session(engine) as session:
