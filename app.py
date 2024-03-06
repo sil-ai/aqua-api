82068d103a337f838491e52d3bcb47e93bf696b9
aquintero <alejandro_quintero@sil.org>
2024-03-06 15:57:41 -0700

    update

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ __version__ = 'v1'
 
 import fastapi
 from fastapi.openapi.utils import get_openapi
-from bible_routes.v1.language_routes import router as language_router_v1
+# from bible_routes.v1.language_routes import router as language_router_v1
 from bible_routes.v2.language_routes import router as language_router_v2
 from bible_routes.v3.language_routes import router as language_router_v3
-from bible_routes.v1.version_routes import router as version_router_v1
+# from bible_routes.v1.version_routes import router as version_router_v1
 from bible_routes.v2.version_routes import router as version_router_v2
 from bible_routes.v3.version_routes import router as version_router_v3
-from bible_routes.v1.revision_routes import router as revision_router_v1
+# from bible_routes.v1.revision_routes import router as revision_router_v1
 from bible_routes.v2.revision_routes import router as revision_router_v2
 from bible_routes.v3.revision_routes import router as revision_router_v3
-from bible_routes.v1.verse_routes import router as verse_router_v1
+# from bible_routes.v1.verse_routes import router as verse_router_v1
 from bible_routes.v2.verse_routes import router as verse_router_v2
 from bible_routes.v3.verse_routes import router as verse_router_v3
-from assessment_routes.v1.assessment_routes import router as assessment_router_v1
+# from assessment_routes.v1.assessment_routes import router as assessment_router_v1
 from assessment_routes.v2.assessment_routes import router as assessment_router_v2
 from assessment_routes.v3.assessment_routes import router as assessment_router_v3
-from review_routes.v1.results_routes import router as results_router_v1
+# from review_routes.v1.results_routes import router as results_router_v1
 from review_routes.v2.results_routes import router as results_router_v2
 from review_routes.v3.results_routes import router as results_router_v3
 from security_routes.auth_routes import router as security_router 
