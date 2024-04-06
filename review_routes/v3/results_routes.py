0501e4ccad248d6edaf31c43ce17511c6c5dcb6a
Mark <woodwardmw@gmail.com>
2024-04-05 20:27:22 -0700

    Missing Words Optimizations

--- review_routes/v3/results_routes.py
+++ review_routes/v3/results_routes.py
@@ -458,8 +458,6 @@ async def build_missing_words_main_query(
     Raises:
         HTTPException: If no completed assessment is found for the provided revision_id and reference_id.
     """
-    start = time.time()
-
     main_assessment = await db.execute(
         select(Assessment)
         .filter(
@@ -477,21 +475,6 @@ async def build_missing_words_main_query(
             detail="No completed assessment found for the given revision_id and reference_id",
         )
 
-    print(f'{time.time() - start} seconds to get main assessment')
-    
-    # Configure the main query for missing words
-    # main_assessment_query = select(
-    #     AlignmentTopSourceScores.id,
-    #     AlignmentTopSourceScores.book,
-    #     AlignmentTopSourceScores.chapter,
-    #     AlignmentTopSourceScores.verse,
-    #     AlignmentTopSourceScores.source,
-    #     AlignmentTopSourceScores.score,
-    # ).where(
-    #     AlignmentTopSourceScores.assessment_id == main_assessment.id,
-    #     AlignmentTopSourceScores.score < threshold,
-    # )
-
     main_assessment_query = (
         select(
             AlignmentTopSourceScores.id.label("id"),
@@ -508,8 +491,6 @@ async def build_missing_words_main_query(
         .order_by('id')
     )
 
-    print(f'{time.time() - start} seconds to get main assessment query')
-
     # Apply filters based on optional parameters
     if book:
         main_assessment_query = main_assessment_query.where(AlignmentTopSourceScores.book == book)
@@ -517,14 +498,6 @@ async def build_missing_words_main_query(
         main_assessment_query = main_assessment_query.where(AlignmentTopSourceScores.chapter == chapter)
     if verse:
         main_assessment_query = main_assessment_query.where(AlignmentTopSourceScores.verse == verse)
-    
-    print(f'{time.time() - start} seconds to apply filters')
-
-
-    # Count total matching rows before applying pagination limits
-    # total_rows = await db.scalar(select(func.count()).select_from(main_assessment_query.subquery()))
-
-    # print(f'{time.time() - start} seconds to get total rows')
 
     return main_assessment_query, main_assessment.id
 
@@ -876,11 +849,7 @@ async def get_missing_words(
     book: Optional[str] = None,
     chapter: Optional[int] = None,
     verse: Optional[int] = None,
-
-    page: Optional[int] = None,
-    page_size: Optional[int] = None,
     db: AsyncSession = Depends(get_db),
-
     current_user: UserModel = Depends(get_current_user),
 ):
     """
@@ -928,8 +897,6 @@ async def get_missing_words(
     same_version_results = await db.execute(same_version_query)
     ids_with_same_version = [result.id for result in same_version_results.all()]
     baseline_ids = [id for id in baseline_ids if id not in ids_with_same_version]
-    print(f'Filtered baseline ids, time: {time.time() - start}')
-
 
     # Initialize base query
     (
@@ -944,14 +911,11 @@ async def get_missing_words(
         verse,
         db,
     )
-    print(f'Initialized base query, time: {time.time() - start}')
-    print(f'{str(main_assessment_query)=}')
     main_assessment_results = await db.execute(main_assessment_query)
     main_assessment_results = main_assessment_results.all()
     df_main = pd.DataFrame(main_assessment_results)
     total_count = len(df_main)
 
-    print(f'Executed main assessment query, time: {time.time() - start}')
     if baseline_ids:
         baseline_assessment_query = await build_missing_words_baseline_query(
         reference_id,
@@ -981,7 +945,6 @@ async def get_missing_words(
         joined_df = pd.merge(
             df_main, df_baseline, on=["book", "chapter", "verse", "source"], how="left"
         )
-        print(f'Merged dataframes, time: {time.time() - start}')
         joined_df['flag'] = (joined_df['baseline_score'] > 0.35) & (joined_df['baseline_score'] > 5 * joined_df['score'])
         df = joined_df.reset_index()
     
@@ -991,7 +954,6 @@ async def get_missing_words(
         df['target'] = df.apply(lambda x: [], axis=1)
 
     result_list = []
-    print(f'Flagged data, time: {time.time() - start}')
 
     for _, row in df.iterrows():
         # Constructing the verse reference string
@@ -1013,8 +975,6 @@ async def get_missing_words(
             flag=row["flag"],
         )
         result_list.append(result_obj)
-
-    # print(f'Constructed results, time: {time.time() - start}')
 
     return {"results": result_list, "total_count": total_count}
 
