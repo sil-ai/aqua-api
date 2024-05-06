c28b00a832285d72ab05e81b72b7bcd285ce3d7f
aquintero <alejandro_quintero@sil.org>
2024-05-06 15:10:11 -0700

    Change alembic

--- alembic/migrations/versions/63a9ad3b1b82_init_migration.py
+++ alembic/migrations/versions/63a9ad3b1b82_init_migration.py
@@ -65,7 +65,6 @@ def upgrade() -> None:
     sa.Column('forward_translation_id', sa.Integer(), nullable=True),
     sa.Column('back_translation_id', sa.Integer(), nullable=True),
     sa.Column('machine_translation', sa.Boolean(), nullable=True),
-    sa.Column('is_reference', sa.Boolean(), nullable=True),
     sa.Column('deleted', sa.Boolean(), nullable=True),
     sa.Column('deletedAt', sa.TIMESTAMP(), nullable=True),
     sa.ForeignKeyConstraint(['back_translation_id'], ['bible_version.id'], ),
